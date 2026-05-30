0fdaa4a78b4f8dcaf8cbcbe0859e0f6cfaf233f4
ARNEL UY <129602037+zaratosheva1503@users.noreply.github.com>
2026-05-30 19:40:47 +0500

    chore: remove dev script check2.js

--- check2.js
+++
@@ -1,9 +0,0 @@
-﻿const fs = require('fs');
-
-const scriptJs = fs.readFileSync('crazygamesport/script.js', 'utf8');
-const appExtras = fs.readFileSync('crazygamesport/app-extras.js', 'utf8');
-
-if (scriptJs.includes('const items = [];\n  const items = [];')) {
-    console.log('Duplicate items array');
-}
-
